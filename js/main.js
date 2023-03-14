2b07b68f67f090ecb4b45544cfa3f8971a423088
ErikaGalanCont <102776229+ErikaGalanCont@users.noreply.github.com>
2023-03-14 17:58:42 -0500

    areaLateral

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
-
-let catetoA = parseFloat(prompt("Introduce la longitud del cateto A: "));
-let catetoB = parseFloat(prompt("Introduce la longitud del cateto B: "));
+//Desarrolle un algoritmo que permita determinar el área lateral y volumen de un cilindro dado su radio (R) y altura (H). Formula: (VOL = π * R^2 * H), (AREA_LATERAL = 2 * π * R * H)
 
 
-let hipotenusa = (catetoA ** 2 + catetoB ** 2) ** 0.5;
+let pi = Math.PI;
+console.log(pi);
 
-hipotenusa = hipotenusa.toFixed(2);
-console.log(`La longitud de la hipotenusa es: ${hipotenusa}`);
+let radio = prompt("Introduce el radio del cilindro");
+let altura = prompt("Introduce la altura del cilindro");
+
+let volumen = pi * radio ** 2 * altura;
+let areaLateral = 2 * pi * radio * altura;
+
+console.log("El volumen del cilindro es: " + volumen);
+console.log("El área lateral del cilindro es: " + areaLateral);
+
+
